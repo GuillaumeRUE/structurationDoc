975e1d874893ddb224a317bd4d36430f7235bc1d
Guillaume <grue57@yahoo.fr>
2019-04-03 08:19:37 +0200

    Premier commit 2

---
+++ src/Transformer.java
@@ -0,0 +1,32 @@
+import javax.xml.transform.*;
+import javax.xml.transform.stream.*;
+import java.io.*;
+
+public class Transformer {
+
+	public static void main(String[] args) throws TransformerConfigurationException {
+		
+		String xslfile = "src/testXSL.xsl";
+		String infile = "src/test.xml";
+		String outfile = "src/out.xml";
+		
+		StreamSource xslcode = new StreamSource(new File(xslfile));
+		StreamSource input = new StreamSource(new File(infile));
+		StreamResult output = new StreamResult(new File(outfile));
+		
+		TransformerFactory tf = TransformerFactory.newInstance();
+		
+		javax.xml.transform.Transformer trans;
+		try {
+			trans = tf.newTransformer(xslcode);
+			trans.transform(input,output);
+			
+		} 
+		catch (TransformerException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
---
+++ src/testXSL.xsl
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+	<xsl:output method="xml" indent="yes"/>
+	<xsl:template match="/">
+		<students>
+			<xsl:apply-templates/>
+		</students>
+	</xsl:template>
+	<xsl:template match="courses/course/students/student">
+		<student>
+			<xsl:attribute name="id"><xsl:value-of select="id"/></xsl:attribute>
+			<firstName>
+				<xsl:value-of select="firstName"/>
+			</firstName>
+			<lastName>
+				<xsl:value-of select="lastName"/>
+			</lastName>
+			<username>
+				<xsl:value-of select="username"/>
+			</username>
+		</student>
+	</xsl:template>
+</xsl:stylesheet>
